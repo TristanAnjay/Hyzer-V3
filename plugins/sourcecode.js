00330c2cbec2204c86017dffa2444ca61a274b41
TristanAnjay <102612033+TristanAnjay@users.noreply.github.com>
2022-05-01 14:06:15 +0700

    Update sourcecode.js

--- plugins/sourcecode.js
+++ plugins/sourcecode.js
@@ -1,8 +1,7 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => conn.sendButtonLoc(m.chat, await (await fetch(fla + 'Script')).buffer(), `
-Lagi Cari Script ya ??
-Nih script botnya..\nMediafire : https://bit.ly/3usJcqJ ( For Termux )\nGithub : https://github.com/davidpangrib001/Hyzer-V3\n_*Jangan Lupa Kasih Starsnya ya..*_
-`.trim(), '❖ One', 'Cara Lewatin-nya gimana ??', 'yntkts')
+Private Bro, Ga Buat Public*_
+`.trim(), '❖ One', 'Loh Kok Gitu???', 'yntkts')
 handler.help = ['sourcecode']
 handler.tags = ['info']
 handler.command = /^(sourcecode|sc|scbot|script|github)$/i
